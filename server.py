4d39a52f027ecbb4d5baee16e9d7cd9b85f89150
rpanczer <bobby.panczer@arcadis-us.com>
2017-09-02 20:05:31 -0700

    GET - artists

--- server.py
+++ server.py
@@ -7,36 +7,27 @@ Author: Bobby Panczer 8/31/2017
 ----------------------------------------------------------
 """
 
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
-from flask.ext.jsonpify import jsonify
 
-# Connect to db, assign api
+# Connect to db
 db_connect = create_engine('sqlite:///albums.db')
 app = Flask(__name__)
-app = api(app)
+api = Api(app)
 
 
 # Create API resources
-class Collection(Resource):
+class Artists(Resource):
     def get(self):
         conn = db_connect.connect()
-        query_db = conn.execute("select * from album")
-        return {'collection': [i[0] for i in query_db.cursor.fetchall()]}
+        query_db = conn.execute("SELECT DISTINCT artist FROM album")
+        return {'artists': [i[0] for i in query_db.cursor.fetchall()]}
 
-
-class Albums(Resource):
-    def get(self, artist):
-        conn = db_connect.connect()
-        query_db = conn.execute("select album from album where artist =" % str(artist))
-        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query_db.cursor]}
-        return jsonify(result)
 
 # Create API routes
-api.add_resource(Collection, '/collection')
-api.add_resource(Albums, '/albums/<artist>')
+api.add_resource(Artists, '/artists')
 
 # Run check and port
 if __name__ == '__main__':
