c97f353ec75bcc311d50c3bc70d3d0b8e25348ba
rpanczer <bobby.panczer@arcadis-us.com>
2017-09-03 10:08:00 -0700

    test for SQL injection and prevent

--- server.py
+++ server.py
@@ -18,6 +18,14 @@ api = Api(app)
 
 
 # Create API resources
+class Albums(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query_db = conn.execute("SELECT DISTINCT album FROM album")
+        result = jsonify({'album list': [i[0] for i in query_db.cursor.fetchall()]})
+        return result
+
+
 class Artists(Resource):
     def get(self):
         conn = db_connect.connect()
@@ -29,12 +37,36 @@ class Artists(Resource):
 class Artistdetails(Resource):
     def get(self, artist_name):
         conn = db_connect.connect()
+        # Protect against SQL injection
+        restricted_char = "!=<>*0&|"
+        for char in restricted_char:
+            artist_name = artist_name.replace(char, "")
         query_db = conn.execute("SELECT DISTINCT album FROM album WHERE artist='{0}'".format(artist_name.title()))
         result = jsonify({'artist album list': [i[0] for i in query_db.cursor.fetchall()]})
         return result
 
+
+class Genreyear(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query_db = conn.execute("SELECT COUNT(album) as count, year FROM album GROUP BY year ORDER BY count desc")
+        result = jsonify({'genre year count': [i[0] for i in query_db.cursor.fetchall()]})
+        return result
+
+
+class Genrenum(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query_db = conn.execute("SELECT genre, count(album) as count FROM album GROUP BY genre ORDER BY count desc")
+        result = jsonify({'genre album count': [i[0] for i in query_db.cursor.fetchall()]})
+        return result
+
 # Create API routes
-api.add_resource(Artists, '/artists')
+api.add_resource(Albums, '/albums')
+api.add_resource(Genreyear, '/albums/yr')
+api.add_resource(Genrenum, '/albums/num')
+
+api.add_resource(Artists, '/artist')
 api.add_resource(Artistdetails, '/artist/<string:artist_name>')
 
 # Run check and port
